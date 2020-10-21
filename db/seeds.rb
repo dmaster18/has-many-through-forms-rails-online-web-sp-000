c7c009ed037db9b920b3e1ded7c73891f57e80c7
David Strauch <dmaster18@gmail.com>
2020-10-21 17:57:20 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Post.create(title: "Fast Cars", content: "Ferraris, Lamborghinis, Astons")
+
+Post.create(title: "Japanese Cars", content: "Honda, Nissan, Toyota")
+
+Category.create(name: "Autos")
+
+Post.create(title: "Shirts", content: "Ralph Lauren, Calvin Klein")
+
+Category.create(name: "Clothes")
+
+Post.create(title: "Fruits", content: "Apples, Bananas, Strawberries")
+Post.create(title: "Meats", content: "Beef, Pork, Chicken")
+
+Category.create(name: "Food")
+
+Post.create(title: "German Cars", content: "BMW, Mercedes-Benz, Audi, Porsche" )
+Post.create(title: "American Cars", content: "Ford, Chevy, Buick, Dodge")
+
